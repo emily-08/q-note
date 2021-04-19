a43d1130d96b998c22a3f062240f8989c1b59eb0
emily-08 <applesuzuki08@iCloud.com>
2021-04-19 19:49:54 +0900

    Userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :memos
   has_many :genres
 
-  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :nickname, presence: true
   validates :password, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: '半角英数字混合で入力してください' }
 end
